349e4d35234967bcaadb64191306d19b0afdb73c
Ramana Nagasamudram <rnagasam@stevens.edu>
2018-03-16 15:26:51 -0400

    very simple knn classifier. needs testing

--- knn.scm
+++ knn.scm
@@ -2,6 +2,12 @@
 
 (load "mat-mit")
 
+(define (vector-zip v1 v2)
+  (make-initialized-vector
+   (min (vector-length v1) (vector-length v2))
+   (lambda (i)
+     (cons (vector-ref v1 i) (vector-ref v2 i)))))
+
 (define sample-data-set
   (list->mat '((1 1.1)
 	       (1 1)
@@ -26,6 +32,22 @@
   ;; data-set : matrix of training samples (n x m)
   ;; labels : a vector of labels (n x 1)
   ;; k : # of nearest neighbors
-  (let ((d (calculate-distances input data-set)))
-    d))
+  (let* ((d (calculate-distances input data-set))
+	 (dist-labl (vector-zip d labels))
+	 (sorted-dist-labl (sort! dist-labl
+				  (lambda (a b)
+				    (< (car a) (car b)))))
+	 (k-neighbors (vector-head sorted-dist-labl k))
+	 (votes (make-initialized-vector (vector-length k-neighbors)
+				       (lambda (i) (cdr (vector-ref k-neighbors i))))))
+    (most-freq votes)))
 
+(define (most-freq v)
+  (let ((h (make-hash-table)))
+    (for (i from 0 to (vector-length v))
+      (hash-table/modify! h
+			  (vector-ref v i)
+			  0
+			  (lambda (e) (+ e 1))))
+    (caar (sort (hash-table->alist h)
+		(lambda (a b) (> (cdr a) (cdr b)))))))
